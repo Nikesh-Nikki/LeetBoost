a7888bc6a40b580119115f1cdac7f31bd617f243
nikesh <nikesh.tadela@gmail.com>
2024-06-16 17:58:18 +0530

    extracting tokens from localStorage instead of webscraping

--- scripts/tokens.js
+++ scripts/tokens.js
@@ -1,16 +1,34 @@
-export default function getTokens(){
-    const allLines = document.getElementsByClassName('view-line')
-    
-    const tokens = new Set()
-
-    Array.from(allLines).forEach(
-        (line) => {
-            const text = line.textContent
-            const words = Array.from(text.matchAll(/\w+/g))
-            words.forEach(
-                (word) => tokens.add(word[0])
-            )
+function getQuestionNumber(){
+    const allAnchors = document.querySelectorAll('a')
+    let questionNumber;
+    Array.from(allAnchors).forEach(
+        anchor => {
+            if(/[0-9]+\. [a-zA-Z ]+/.test(anchor.innerText))
+                questionNumber = anchor.innerText.match(/[0-9]+/)
         }
+    )
+    return questionNumber[0]
+}
+
+function findKeyForLocalStorage( questionNumber ){
+    for(let i in localStorage) {
+        if(i.startsWith(questionNumber+'_') && /[0-9]+_[0-9]+_\w+$/.test(i))
+            return i
+    }
+}
+
+function getTokens(){
+
+    if(! this.keyForSolution ) this.keyForSolution = findKeyForLocalStorage(getQuestionNumber())
+
+    const tokens = new Set()
+    let code = localStorage[this.keyForSolution]
+    // deleting all new line chars
+    code = code.replaceAll(/\\n/g,' ')
+    const words = code.matchAll(/[a-zA-Z_]+/g)
+
+    words.forEach(
+        word => tokens.add(word[0])
     )
 
     return tokens
