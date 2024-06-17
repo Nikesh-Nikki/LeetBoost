6c96e0e2b236f53ed1710092f50af0de10c7e050
nikesh <nikesh.tadela@gmail.com>
2024-06-17 17:23:20 +0530

    set an interval that sets the position of suggestion box

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import SuggestBox from './SuggestBox.jsx'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function App(){
 
     const [isActive , setIsActive] = useState(false)
+    const [position,setPosition] = useState()
+
+    useEffect(
+        () => {
+            const textarea = document.querySelector('#editor textarea')
+            const interval = setInterval(
+                ()=>{
+                    setPosition(
+                        {
+                            x : textarea?.getBoundingClientRect().x , 
+                            y : textarea?.getBoundingClientRect().y
+                        }
+                    )
+                }
+                ,
+                100
+            )
+            return (()=>clearInterval(interval))
+        }
+        ,
+        []
+    )
 
     // display suggestbox only when active element is editor
 
@@ -18,11 +41,13 @@ export default function App(){
         'blur' , ()=>setIsActive(false)
     )
 
+    
+
     return (
         <SuggestBox 
             hide={!isActive} 
-            left={textarea.getBoundingClientRect().x} 
-            top={textarea.getBoundingClientRect().y+18}
+            left={position?.x} 
+            top={((position?.y) ? position.y+18 : undefined)}
             suggestedWords={['hello','nikki','bucky']}
             />
     )
