b10c5e5966ae0bc21053f388cf1a1ab33c923e97
nikesh <nikesh.tadela@gmail.com>
2024-06-25 21:26:52 +0530

    changed editor ready check

--- scripts/enable.js
+++ scripts/enable.js
@@ -2,18 +2,17 @@ console.log('enabling')
 
 const editorReadyInterval = setInterval(
     ()=>{
-        try {
-            window.monaco.editor.getEditors()[0].updateOptions({
-                suggestOnTriggerCharacters: true,
-                quickSuggestions: true,
-                parameterHints: {
-                    enabled: true
-                }
-            })
-            clearInterval(editorReadyInterval) 
-        } catch(err) {
-
-        }
-    },
+            const textarea = document.querySelector('#editor textarea')
+            if(textarea && textarea == document.activeElement) {
+                window.monaco.editor.getEditors()[0].updateOptions({
+                    suggestOnTriggerCharacters: true,
+                    quickSuggestions: true,
+                    parameterHints: {
+                        enabled: true
+                    }
+                })
+                clearInterval(editorReadyInterval) 
+            }
+        } ,
     100
 )
