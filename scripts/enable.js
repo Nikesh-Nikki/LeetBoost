f2ac744c29962e7b941e39247b79428548dc7742
nikesh <nikesh.tadela@gmail.com>
2024-06-26 13:54:05 +0530

    fixed doubling of tokens error

--- scripts/enable.js
+++ scripts/enable.js
@@ -11,35 +11,12 @@ const editorReadyInterval = setInterval(
                         enabled: true
                     }
                 })
-                const editor = document.getElementById('editor')
-                editor.onkeydown = ()=> makeItems()
+                makeItems()
                 clearInterval(editorReadyInterval) 
             }
         } ,
     100
 )
-
-function getTokens(){
-    const editor = window.monaco.editor.getEditors()[0]
-    const currentLangauge = editor.getModel().getLanguageId()
-    // they have a good default context aware intellisense, i dont have to create items
-    if(currentLangauge == 'javascript' || currentLangauge == 'typescript') return []
-    // get the entire code and the extract tokens
-    const code = editor.getValue()
-    const words = Array.from(code.matchAll(/\b[a-zA-Z\_]\w+/g))
-    // also get current word and exclude it from the list
-    const currentPosition = editor.getPosition()
-    let wordAtPosition = editor.getModel().getWordAtPosition(currentPosition)
-    // ahaa now lets filter out
-    const set = new Set()
-    words.forEach(
-        (word) => {
-            if(wordAtPosition == null || word[0] != wordAtPosition.word) set.add(word[0])
-            else wordAtPosition.word = '-'
-        }
-    )
-    return Array.from(set)
-}
 
 function getSnippets() {
     return (
@@ -57,30 +34,17 @@ function getSnippets() {
 }
 
 function makeItems(){
-    const tokens = getTokens()
     const snippets = getSnippets()
     monaco.languages.registerCompletionItemProvider('*', {
         provideCompletionItems: () => {
             const suggestions = [
-                ...tokens.map(
-                    token => {
-                        return {
-                            label: token,
-                            kind: monaco.languages.CompletionItemKind.Text,
-                            insertText: token,
-                            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-                            documentation: 'Recomending because it is already typed by you'
-                        }
-                    }
-                ),
                 ...snippets.map(
                     snippet => {
                         return {
                             label: snippet.trigger,
                             kind: monaco.languages.CompletionItemKind.Snippet,
                             insertText: snippet.snippet,
-                            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-                            documentation: 'Recomending because it is already typed by you'
+                            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
                         }
                     }
                 )
