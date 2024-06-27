de8d206b6ede1b81670771c315d72d82b271d2ba
nikesh <nikesh.tadela@gmail.com>
2024-06-27 16:43:37 +0530

    created Editor component. users can create new snippets.

---
+++ src/components/Editor.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function Editor({snippets, editIndex, handleSubmit, handleDiscard}){
+    const [triggerState , setTriggerState] = useState('')
+    const [snippetState , setSnippetState] = useState('')
+    const [errorState,setErrorState] = useState()
+
+    function validate(){
+        if(triggerState.length === 0) setErrorState('trigger cant be empty')
+        else if(snippetState.length === 0) setErrorState('snippet cant be empty')
+        else {
+            if(
+                editIndex === undefined
+                ||
+                triggerState !== editIndex
+            ) {
+                for(let i = 0; i < snippets.length; i++){
+                    if(snippets[i].trigger === triggerState){
+                        setErrorState(`${triggerState} trigger already exists..!!`) 
+                        return
+                    }
+                }
+            } 
+            handleSubmit(editIndex,triggerState,snippetState)
+        }
+    }
+
+    return (
+        <div id = "editor">
+            <div id = 'error'>{errorState}</div>
+            <input type="text" value = {triggerState} onChange={(e)=>setTriggerState(e.target.value)}/>
+            <textarea
+            value = {snippetState}
+            onChange={e => setSnippetState(e.target.value)}
+            >
+            </textarea>
+            <button id = "submit" onClick={validate}>
+                Submit
+            </button>
+            <button id="discard" onClick={handleDiscard}>
+                Discard
+            </button>
+        </div>
+    )
+}
--- src/components/SnippetsContainer.jsx
+++ src/components/SnippetsContainer.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Snippet from "./Snippet.jsx";
+import Editor from "./Editor.jsx";
 
 export default function SnippetsContainer(){
 
     const [snippets,setSnippets] = useState([])
+    const [edit , setEdit] = useState(
+        {
+            editing : false
+        }
+    )
 
     useEffect(
         () => {
@@ -14,14 +20,52 @@ export default function SnippetsContainer(){
         []
     )
 
+    function handleSubmit(editIndex, newTrigger, newSnippet){
+        let newSnippets = snippets
+        if(editIndex !== undefined) newSnippets.splice(newSnippets.indexOf(s => s.trigger == editIndex),1)
+        newSnippets.push({
+            trigger : newTrigger,
+            snippet : newSnippet
+        })
+        setSnippets(newSnippets)
+        chrome.storage.local.set({snippets : newSnippets})
+        setEdit(
+            {
+                editing : false
+            }
+        )
+    }
+
+    function handleDiscard(){
+        setEdit(
+            {
+                editing : false
+            }
+        )
+    }
+
     return (
         <div id = "snippets-container">
             {
-                snippets.map(
-                    (snippet) => {
-                        console.log(snippet)
-                       return <Snippet key={snippet.trigger} trigger={snippet.trigger} snippet={snippet.snippet} />
-                    }
+                edit.editing ? <Editor editIndex={edit.editIndex} snippets={snippets} handleSubmit={handleSubmit} handleDiscard={handleDiscard}/> 
+                : 
+                (
+                    <>
+                        <div id = 'snippet-header'>
+                            <h2>Snippets</h2>
+                            <button onClick={()=>setEdit({editing : true})}>
+                                Add
+                            </button>
+                        </div>
+                        {
+                            snippets.map(
+                                (snippet) => {
+                                    console.log(snippet)
+                                return <Snippet key={snippet.trigger} trigger={snippet.trigger} snippet={snippet.snippet} />
+                                }
+                            )
+                        }
+                    </>
                 )
             }
         </div>
