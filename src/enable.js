a1ae22d4256ef82b9a85727edf9d063faa59ee8f
Abhinav Bolla <abhinavbolla@gmail.com>
2026-06-25 13:49:52 +0530

    Fix leetcode editor detection

--- src/enable.js
+++ src/enable.js
@@ -1,4 +1,14 @@
-window.monaco.editor.getEditors()[0].updateOptions({
+function applyEditorOptions(options) {
+    const editors = window.monaco?.editor?.getEditors?.() || []
+    if (editors.length === 0) {
+        setTimeout(() => applyEditorOptions(options), 100)
+        return
+    }
+
+    editors.forEach((editor) => editor.updateOptions(options))
+}
+
+applyEditorOptions({
     suggestOnTriggerCharacters: true,
     quickSuggestions: true,
     parameterHints: {
